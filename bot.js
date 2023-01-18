bb9bdf6095029cb97313e23db8e47157ae128845
Shriyansh Aggarwal <65995690+God-Osiris@users.noreply.github.com>
2023-01-18 13:58:53 +0530

    Added port

--- bot.js
+++ bot.js
@@ -7,6 +7,14 @@
 
 // Declare constants which will be used throughout the bot.
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
 const fs = require("fs");
 const {
 	Client,
